78fc38e5b35db1057c467153685afb5818ec5b34
Nacho | Jordan <36382273+Nacholebraa@users.noreply.github.com>
2024-02-24 07:52:29 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,10 +98,6 @@ const config = {
                 label: 'Staff',
                 to: '/staff',
               },
-              {
-                label: 'Government',
-                to: '/government',
-              },
             ],
           },
           {
