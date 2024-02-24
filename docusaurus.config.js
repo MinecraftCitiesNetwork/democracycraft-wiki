a5a7d44986f3776d1ee4e296e630f240f3325d97
Nacho | Jordan <36382273+Nacholebraa@users.noreply.github.com>
2024-02-24 07:41:23 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,13 +44,6 @@ const config = {
           editUrl:
             'https://github.com/MinecraftCitiesNetwork/democracycraft-wiki/tree/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/MinecraftCitiesNetwork/democracycraft-wiki/tree/main/',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -95,11 +88,19 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'Wiki',
             items: [
               {
                 label: 'Tutorial',
                 to: '/',
+              },
+              {
+                label: 'Staff',
+                to: '/staff',
+              },
+              {
+                label: 'Government',
+                to: '/government',
               },
             ],
           },
